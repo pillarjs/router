113cf3ff3e3c105f234c735f839e9117cf694360
calebmer <calebmeredith8@gmail.com>
2015-10-05 20:53:12 -0400

    Once-ify next method when passed to middleware

---
+++ test/middleware.js
@@ -0,0 +1,41 @@
+
+var Router = require('..')
+var utils = require('./support/utils')
+
+var assert = utils.assert
+var createServer = utils.createServer
+var request = utils.request
+
+describe('middleware', function () {
+  it('cannot call the next function twice', function (done) {
+    var router = Router()
+    var server = createServer(router)
+    var mochaUncaughtException = process.listeners('uncaughtException').pop()
+    // Needed in node 0.10.5+
+    process.removeListener('uncaughtException', mochaUncaughtException)
+
+    router.use(function (req, res, next) {
+      next()
+      next()
+    })
+
+    router.use(function (req, res, next) {
+      res.writeHead(200)
+      res.end('Hooray!')
+    })
+
+    router.use(function () { done(new Error('this should not be called')) })
+
+    function uncaughtException(err) { assert.equal(err.message, 'cannot call next twice') }
+    process.once('uncaughtException', uncaughtException)
+
+    request(server)
+    .get('/')
+    .expect(200, 'Hooray!', function (err) {
+      if (err) { return done(err) }
+      assert.equal(process.listeners('uncaughtException').indexOf(uncaughtException), -1)
+      process.addListener('uncaughtException', mochaUncaughtException)
+      done()
+    })
+  })
+})
