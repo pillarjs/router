eec6c365d2b8cee5d4f01177e714435d6072052f
egydev1990 <egydev1990@gmail.com>
2018-11-04 12:36:58 +0300

    updated index.js as removed && method !== 'HEAD' condtion in line 271

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ Router.prototype.handle = function handle(req, res, callback) {
       }
 
       // don't even bother matching route
-      if (!has_method && method !== 'HEAD') {
+      if (!has_method) {
         match = false
         continue
       }
