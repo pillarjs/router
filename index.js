d3af9e999386b1aaaf835935bdf291ed641b5e9b
Daniel Lando <daniel.sorridi@gmail.com>
2020-05-27 17:15:33 +0200

    feat: removeAllRoutes public method
    This method allows to clear routes stack. As discussed here: https://github.com/expressjs/express/issues/4296

--- index.js
+++ index.js
@@ -436,6 +436,16 @@ Router.prototype.process_params = function process_params(layer, called, req, re
 }
 
 /**
+ * Remove all paths from the current stack
+ *
+ * @public
+ */
+
+Router.prototype.removeAllRoutes = function removeAllRoutes() {
+  this.stack = []
+}
+
+/**
  * Use the given middleware function, with optional path, defaulting to "/".
  *
  * Use (like `.all`) will run for any http METHOD, but it will not add
